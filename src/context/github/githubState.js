e68c9f4177d87571c3db7877b8e0c29ead85e702
y245fan <y245fan@uwaterloo.ca>
2021-01-28 15:54:32 -0500

    add homepage

--- src/context/github/githubState.js
+++ src/context/github/githubState.js
@@ -15,14 +15,24 @@ const GithubState = props => {
 
     const [state, dispatch] = useReducer(GithubReducer, initialState);
 
+    let githubClientId;
+    let githubClientSecret;
+
+    if ( process.env.NODE_ENV !== 'production' ) {
+        githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
+        githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+    } else {
+        githubClientId = process.env.REACT_APP_ID;
+        githubClientSecret = process.env.REACT_APP_SECRET;
+    }
     
     // Search Users
     const searchUsers = async (text) => {
         setLoading();
   
         const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=
-        ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
-        ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+        ${githubClientId}&client_secret=
+        ${githubClientSecret}`);
 
         dispatch({
             type: SEARCH_USERS,
@@ -35,8 +45,8 @@ const GithubState = props => {
         setLoading();
 
         const res = await axios.get(`https://api.github.com/users/${username}?client_id=
-        ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
-        ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+        ${githubClientId}&client_secret=
+        ${githubClientSecret}`);
 
         dispatch({
             type: GET_USER,
@@ -49,8 +59,8 @@ const GithubState = props => {
         setLoading();
     
         const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=
-        ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
-        ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+        ${githubClientId}&client_secret=
+        ${githubClientSecret}`);
 
         console.log(res.data);
     
